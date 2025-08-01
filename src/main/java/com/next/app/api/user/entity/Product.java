3d3534e6528beb2b22d871fc2efd7ed0fb80217b
realrain5538 <bizrealrain@gmail.com>
2025-08-01 14:06:21 +0900

    add product api

---
+++ src/main/java/com/next/app/api/user/entity/Product.java
@@ -0,0 +1,62 @@
+package com.next.app.api.user.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product_realrain")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private BigDecimal price;
+
+    @Column
+    private String description;
+
+    // Constructors
+    public Product() {}
+
+    public Product(String name, BigDecimal price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
+
+
+
+
+
+
